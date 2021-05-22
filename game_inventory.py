6d88020d3265d2325307cacba7d4f870ff4dcdca
WDev-Beginner <76706155+WDev-Beginner@users.noreply.github.com>
2021-05-22 15:53:20 +0200

    implmented the first three function, print, add, remove

--- game_inventory.py
+++ game_inventory.py
@@ -6,17 +6,30 @@
 
 def display_inventory(inventory):
     """Display the contents of the inventory in a simple way."""
-    pass
+    for key, value in inventory.items():
+        print(f'{key}: {value}')
+
 
 
 def add_to_inventory(inventory, added_items):
     """Add to the inventory dictionary a list of items from added_items."""
-    pass
+    for key in added_items:
+        if key in inventory.key():
+            inventory[key]+=1
+            continue
+        inventory[key] = 1
+    return inventory
 
 
 def remove_from_inventory(inventory, removed_items):
     """Remove from the inventory dictionary a list of items from removed_items."""
-    pass
+    for key in removed_items:
+        if key not in inventory.keys():
+            continue
+        inventory[key] -= 1
+        if inventory[key] == 0:
+            del inventory[key] 
+    return inventory
 
 
 def print_table(inventory, order):
@@ -38,3 +51,8 @@ def export_inventory(inventory, filename):
     """Export the inventory into a CSV file."""
 
     pass
+
+
+test_inventory = {'pattern' : 10, 'pattern2' : 50,'pattern1' : 50}
+removed_items = ['pattern3','pattern3']
+display_inventory(remove_from_inventory(test_inventory, removed_items))
