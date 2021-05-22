758e3b50e729af82ee4f72dbf803fe96dac7db58
WDev-Beginner <76706155+WDev-Beginner@users.noreply.github.com>
2021-05-22 16:55:02 +0200

    started work on the import inventory function

--- game_inventory.py
+++ game_inventory.py
@@ -1,5 +1,6 @@
 from operator import itemgetter
-import pandas as pd 
+import pandas as pd
+import numpy as np
 
 # This is the file where you must work.
 # Write code in the functions (and create new functions) so that they work
@@ -9,15 +10,14 @@ import pandas as pd
 def display_inventory(inventory):
     """Display the contents of the inventory in a simple way."""
     for key, value in inventory.items():
-        print(f'{key}: {value}')
-
+        print(f"{key}: {value}")
 
 
 def add_to_inventory(inventory, added_items):
     """Add to the inventory dictionary a list of items from added_items."""
     for key in added_items:
         if key in inventory.key():
-            inventory[key]+=1
+            inventory[key] += 1
             continue
         inventory[key] = 1
     return inventory
@@ -30,7 +30,7 @@ def remove_from_inventory(inventory, removed_items):
             continue
         inventory[key] -= 1
         if inventory[key] == 0:
-            del inventory[key] 
+            del inventory[key]
     return inventory
 
 
@@ -41,27 +41,27 @@ def print_table(inventory, order):
     """
     order_list = ["count,desc", "count,asc"]
     sort_dict = inventory
-    if order in order_list: 
+    if order in order_list:
         if order == "count,asc":
             sort_dict = dict(sorted(inventory.items()))
-        else: 
-            sort_dict = dict(sorted(test_inventory.items(),reverse=True))        
+        else:
+            sort_dict = dict(sorted(test_inventory.items(), reverse=True))
 
     horiz_line = "-----------------"
     print(horiz_line)
-    print('{:<10s} | {:>4s}'.format(" item name", "count"))
+    print("{:<10s} | {:>4s}".format(" item name", "count"))
     print(horiz_line)
     for key, value in sort_dict.items():
-        print('{:>10s} | {:>4d}'.format(key, value))
-    print(horiz_line) 
-
-
+        print("{:>10s} | {:>4d}".format(key, value))
+    print(horiz_line)
 
 
 def import_inventory(inventory, filename):
     """Import new inventory items from a CSV file."""
+    csv_inventory = pd.read_csv(filename, delimiter=",")
+    df = list(csv_inventory)
 
-    pass
+    print(df)
 
 
 def export_inventory(inventory, filename):
@@ -70,12 +70,11 @@ def export_inventory(inventory, filename):
     pass
 
 
-test_inventory = {'pattern' : 10, 'pattern2' : 50,'pattern1' : 30}
+test_inventory = {"pattern": 10, "pattern2": 50, "pattern1": 30}
 """removed_items = ['pattern3','pattern3']
 display_inventory(remove_from_inventory(test_inventory, removed_items)) """
 
-print_table(test_inventory,"")
+print_table(test_inventory, "")
 print_table(test_inventory, "count,asc")
 print_table(test_inventory, "count,desc")
-
-
+import_inventory(test_inventory, "test_inventory.csv")
